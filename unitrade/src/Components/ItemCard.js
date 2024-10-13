94c6416ecc8ba33a311ea0df6320d860b5cf57ac
Anusha <anusha.k@objectsourceinc.com>
2024-10-13 08:59:59 -0500

    color changed

--- unitrade/src/Components/ItemCard.js
+++ unitrade/src/Components/ItemCard.js
@@ -7,7 +7,7 @@ const ItemCard = ({ product, onClick }) => {
       <div className="card h-100 shadow-sm" onClick={onClick} style={{ cursor: 'pointer', borderColor: "#500221", borderRadius: "20px" }}>
         <img src={product.image} className="card-img-top" alt={product.name} style={{ height: "70%", borderTopLeftRadius: "20px", borderTopRightRadius: "20px" }} />
         <div className="card-body text-center" style={{ position: "absolute", bottom: "0", marginBottom: "10px", cursor: "pointer" }}>
-          <h5 className="card-title">{product.name}</h5>
+          <h5 className="card-title" style={{ color: "#500221", fontWeight: "700",fontFamily:"sans-serif" }}>{product.name}</h5>
           <p className="card-text">${product.price}</p>
         </div>
       </div>
